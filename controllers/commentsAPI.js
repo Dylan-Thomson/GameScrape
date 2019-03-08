891350c7cf153799550d1086817f583445f42690
Dylan Thomson <djthomson88@gmail.com>
2019-03-08 11:25:45 -0500

    Start adding routes/logic for comments, comment button

---
+++ controllers/commentsAPI.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const router = express.Router();
+const db = require('../models');
+
+router.get('/', (req, res) => {
+  db.Comment.find({}).then((dbComment) => {
+    res.json(dbComment);
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  db.Comment.find({ _id: req.params.id }).then((dbComment) => {
+    res.json(dbComment);
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
+router.post('/', (req, res) => {
+  db.Comment.create(req.body).then((dbComment) => {
+    res.json(dbComment);
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
+module.exports = router;
